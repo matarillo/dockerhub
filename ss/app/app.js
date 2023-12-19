efaa189e536c8c702e009c639b7b6f965f1dc84f
matarillo <kentaro.inomata@gmail.com>
2023-12-19 21:02:33 +0900

    Add try-finally

--- ss/app/app.js
+++ ss/app/app.js
@@ -10,28 +10,30 @@ const screenshot = async (req, res, next) => {
             executablePath: '/bin/google-chrome',
             args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=480,800']
         });
-        const page = await browser.newPage();
-        await page.setViewport({
-            width: 480,
-            height: 800,
-        });
-        const url = req.query.url;
-        if (!url || url.trim().length == 0) {
-            res.status(400).send('request error');
-            return;
+        try {
+            const page = await browser.newPage();
+            await page.setViewport({
+                width: 480,
+                height: 800,
+            });
+            const url = req.query.url;
+            if (!url || url.trim().length == 0) {
+                res.status(400).send('request error');
+                return;
+            }
+            await page.goto(url);
+            await page.waitForSelector('div.detailed-status__wrapper');
+            await new Promise(r => setTimeout(r, 2000));
+
+            const clip = await page.evaluate(s => {
+                const el = document.querySelector(s);
+                const { width, height, top: y, left: x } = el.getBoundingClientRect();
+                return { width, height, x, y };
+            }, 'div.detailed-status__wrapper');
+            await page.screenshot({ clip, path: 'ss.png' });
+        } finally {
+            await browser.close();
         }
-        await page.goto(url);
-        await page.waitForSelector('div.detailed-status__wrapper');
-        await new Promise(r => setTimeout(r, 2000));
-
-        const clip = await page.evaluate(s => {
-            const el = document.querySelector(s);
-            const { width, height, top: y, left: x } = el.getBoundingClientRect();
-            return { width, height, x, y };
-        }, 'div.detailed-status__wrapper');
-        await page.screenshot({ clip, path: 'ss.png' });
-        await browser.close();
-
         const content = await fs.readFile('ss.png');
         res.writeHead(200, {'Content-Type': 'image/png' });
         res.end(content);
